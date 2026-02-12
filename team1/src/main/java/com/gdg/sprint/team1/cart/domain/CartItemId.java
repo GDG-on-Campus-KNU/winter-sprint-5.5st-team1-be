cd613ae190fe3e465f89e27e5fbb01e75bbc6c10
sinyeowon <ioohyou@knu.ac.kr>
2026-02-12 16:12:59 +0900

    feat: JPA 의존성 추가 및 CartItemId 복합키 엔티티 구현

---
+++ team1/src/main/java/com/gdg/sprint/team1/cart/domain/CartItemId.java
@@ -0,0 +1,40 @@
+package com.gdg.sprint.team1.cart.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class CartItemId implements Serializable {
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "product_id")
+    private Integer productId;
+
+    protected CartItemId() {}
+
+    public CartItemId(Integer userId, Integer productId) {
+        this.userId = userId;
+        this.productId = productId;
+    }
+
+    public Integer getUserId() { return userId; }
+    public Integer getProductId() { return productId; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CartItemId)) return false;
+        CartItemId that = (CartItemId) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, productId);
+    }
+}
