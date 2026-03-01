e25df7bb509cadcbea20d956193b306cd294f027
kimjhyun0627 <kimjhyun0627@gmail.com>
2026-03-02 01:20:02 +0900

    fix: mockitoBean 패키지 로드 오류 수정

--- team1/src/test/java/com/gdg/sprint/team1/Team1ApplicationTests.java
+++ team1/src/test/java/com/gdg/sprint/team1/Team1ApplicationTests.java
@@ -2,14 +2,14 @@ package com.gdg.sprint.team1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class Team1ApplicationTests {
 
-	@MockBean
+	@MockitoBean
 	S3Client s3Client;
 
 	@Test
