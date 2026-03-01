c02d4db1cf5a12fea477b08b2b176c9d10614675
kimjhyun0627 <kimjhyun0627@gmail.com>
2026-03-02 01:17:11 +0900

    fix: 테스트에 @MockBean S3Client 추가

--- team1/src/test/java/com/gdg/sprint/team1/Team1ApplicationTests.java
+++ team1/src/test/java/com/gdg/sprint/team1/Team1ApplicationTests.java
@@ -2,9 +2,15 @@ package com.gdg.sprint.team1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class Team1ApplicationTests {
+
+	@MockBean
+	S3Client s3Client;
 
 	@Test
 	void contextLoads() {
